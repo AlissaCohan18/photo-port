7bee369c14eea04ee4ba73126ac2e4d409f5d138
Alissa Cohan <alissacohan18@gmail.com>
2023-03-16 19:39:02 -0400

    add and update test for components

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -1,39 +1,41 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import Nav from "..";
+// __tests__/Nav.test.js with hard coded categories
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
 
 afterEach(cleanup);
 
-describe("Nav component", () => {
-  // baseline test
-  it("renders", () => {
+describe('Nav component', () => {
+    // baseline test
+  it('renders', () => {
     render(<Nav />);
   });
 
   // snapshot test
-  it("matches snapshot", () => {
+  it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
 
     expect(asFragment()).toMatchSnapshot();
   });
-});
+})
 
 //check to see if the emoji, 📸, is visible
-describe("emoji is visible", () => {
-  it("inserts emoji into the h2", () => {
+describe('emoji is visible', () => {
+  it('inserts emoji into the h2', () => {
     const { getByLabelText } = render(<Nav />);
 
-    expect(getByLabelText("camera")).toHaveTextContent("📸");
+    expect(getByLabelText('camera')).toHaveTextContent('📸');
   });
-});
+})
 
 //verify that the text content is being inserted into the <a> tags
-describe("links are visible", () => {
-  it("inserts text into the links", () => {
+describe('links are visible', () => {
+  it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
 
-    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-    expect(getByTestId("about")).toHaveTextContent("About me");
+    expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+    expect(getByTestId('about')).toHaveTextContent('About me');
   });
-});
+
+})
