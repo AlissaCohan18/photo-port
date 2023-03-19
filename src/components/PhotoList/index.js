ddcf63b7beb677ca1fdb8fa9deb893224f633bf3
Alissa Cohan <alissacohan18@gmail.com>
2023-03-18 21:17:01 -0400

    added modal for selected picture

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentPhoto, setCurrentPhoto] = useState();
+
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -123,20 +127,29 @@ console.log(`../../assets/small/${category}/${0}.jpg`)
   //goes thru each photo in the photos array to find every photo that matches the category selected
   //If a photo matches, it's returned in an array and assigned to currentPhotos. Then we can map
   //the currentPhotos array to render each photo that matches the category selected by the user
-  const currentPhotos = photos.filter(photo => photo.category === category);
+  const currentPhotos = photos.filter((photo) => photo.category === category);
+
+  const toggleModal = (image, i) => {
+    setCurrentPhoto({ ...image, index: i });
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div>
+      {isModalOpen && (
+        <Modal onClose={toggleModal} currentPhoto={currentPhoto} />
+      )}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-          //the default property is where the image has been saved;
+            //the default property is where the image has been saved;
             //To render the image, the default property must be invoked
             src={require(`../../assets/small/${category}/${i}.jpg`).default}
-             //for accessibility user-assistance devices
+            //for accessibility user-assistance devices
             alt={image.name}
             //attribute value must be a unique string
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
